d0d99643d068b45c9f59d3b9a417cfb113df3669
Zachary <zacharyjolliff15@gmail.com>
2025-04-25 19:41:09 -0400

    output finished

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -3,10 +3,17 @@
 
 function lighton() {
     document.getElementById('light').src='/project/src/images/greenlight.jpg';
+    document.getElementById('onOff on').innerText="On"
+    alert("Light is on!")
+    console.log("light is on")
 }
 
 function lightoff() {
     document.getElementById('light').src='/project/src/images/redlight.jpg';
+    document.getElementById('onOff off').innerText="Off"
+    window.alert("Light is off!")
+    console.log("light is off")
+
 }
 
 function disappear(){
