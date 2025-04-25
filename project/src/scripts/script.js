351ebc9b7cdf57e2ee9fb6a40118264cd15dec7f
Zachary <zacharyjolliff15@gmail.com>
2025-04-25 19:57:25 -0400

    statements done

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,11 +1,15 @@
 //.innerHTML changes everything inside the HTML Box
 //.textContent changes only the text inside the html box
 
+
+let totalClicks = 0;
+
 function lighton() {
     document.getElementById('light').src='/project/src/images/greenlight.jpg';
     document.getElementById('onOff on').innerText="On"
     alert("Light is on!")
     console.log("light is on")
+    clickAdd(1)
 }
 
 function lightoff() {
@@ -13,7 +17,7 @@ function lightoff() {
     document.getElementById('onOff off').innerText="Off"
     window.alert("Light is off!")
     console.log("light is off")
-
+    clickAdd(5)
 }
 
 function disappear(){
@@ -22,4 +26,11 @@ function disappear(){
 
 function reappear(){
     document.getElementById('light').style.display='block';
+}
+
+
+function clickAdd(num){
+    totalClicks = totalClicks + num;
+    console.log(totalClicks)
+    return totalClicks
 }
