24f4be7973c8d5bbab3f80cae3bb8205dc97005c
Zachary <zacharyjolliff15@gmail.com>
2025-04-25 19:12:05 -0400

    we-write inline js as 4 functions

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,4 +1,18 @@
-
 //.innerHTML changes everything inside the HTML Box
 //.textContent changes only the text inside the html box
 
+function lighton() {
+    document.getElementById('light').src='/project/src/images/greenlight.jpg';
+}
+
+function lightoff() {
+    document.getElementById('light').src='/project/src/images/redlight.jpg';
+}
+
+function disappear(){
+    document.getElementById('light').style.display='none';
+}
+
+function reappear(){
+    document.getElementById('light').style.display='block';
+}
