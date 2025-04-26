e1472a9dd492c62cf1e3c616da8b0ddadf82b2c9
Zachary <zacharyjolliff15@gmail.com>
2025-04-26 12:57:45 -0400

    constructor and object learning

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -47,32 +47,16 @@ function clickInfo(){
     console.log(light["size"])
 }
 
-// function Person(first, last) {
-//     this.firstName = first;
-//     this.lastName = last;
-//     this.fullName = function() {
-//       return this.firstName + " " + this.lastName;
-//     };
-//   }
-  
-//   const person1 = new Person("Bob", "Johnson");
-//   const person2 = new Person("Charlie", "Brown");
-  
-//   console.log(person1.fullName()); // Output: Bob Johnson
-//   console.log(person2.fullName()); // Output: Charlie Brown
 
-  
-const person = {
-    firstName: "Alice",
-    lastName: "Smith",
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    },
-    greet: function() {
-      console.log(`Hello, my name is ${this.fullName()}.`);
-    }
-  };
-  
-  console.log(person.fullName()); // Output: Alice Smith
-  person.greet(); // Output: Hello, my name is Alice Smith.
+//Constructer function for dog objects
+function dog(color, weight, kidFriendly) {
+    this.color = color;
+    this.weight = weight;
+    this.kidFriendly = kidFriendly;
+    this.worseThancats= true;
+}
 
+const dog1 = new dog("red",120,false) ;
+const dog2 = { color: "brown", weight: 85, kidFriendly: true };
+const dog3 = { color: "yellow", weight: 45, kidFriendly: false, gay: false };
+const dog4 = new dog("greensih", 200, true, 234, 444, 477) //extra arguements ignored, but accesible
