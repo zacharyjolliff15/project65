411956dbe14ebae1c35adfbf04d6499560f68374
Zachary <zacharyjolliff15@gmail.com>
2025-04-26 13:03:44 -0400

    rollback because i checked out to the wrong branch

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,3 +1,53 @@
+let totalClicks = 0;
+let greenClicks = 0;
+let redClicks = 0;
+
+//save calls to dom, helps memory
+const lightImg = document.getElementById('light');
+const onButton = document.getElementById('onOff on');
+const offButton = document.getElementById('onOff off');
+
+const light = {
+    type: "General Electric",
+    cost: 6.99
+};
+light.size = "medium";
+
+function lighton() {
+    greenClicks++;
+    totalClicks = greenClicks + redClicks;
+    lightImg.src='/project/src/images/greenlight.jpg';
+    // alert("Light is on!")
+    clickInfo()
+    onButton.innerText = greenClicks % 2 === 0 ? "On" : "Press for On";
+};
+
+function lightoff() {
+    redClicks++;
+    totalClicks = greenClicks + redClicks;
+    lightImg.src='/project/src/images/redlight.jpg';
+    // window.alert("Light is off!")
+    clickInfo()
+    offButton.innerText = redClicks % 2 === 0 ? "Off" : "Press for Off";
+};
+
+function disappear(){
+    lightImg.style.display='none';
+};
+
+function reappear(){
+    lightImg.style.display='block';
+};
+
+function clickInfo(){
+    console.log("total green clicks: " + greenClicks)
+    console.log("total red clicks: " + redClicks)
+    console.log("total clicks: " + totalClicks)
+    console.log(light.cost)
+    console.log(light["size"])
+}
+
+
 //Constructer function for dog objects
 function dog(color, weight, kidFriendly) {
     this.color = color;
