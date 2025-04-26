ebb1479e5c1b64e748b59c0279a89e914634a9c6
Zachary <zacharyjolliff15@gmail.com>
2025-04-26 09:07:17 -0400

    my two sum solution

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -47,3 +47,16 @@ function clickInfo(){
     console.log(light["size"])
 }
 
+let nums = [2,5,5,11]
+let target = 10
+let twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target){
+                return console.log([i,j]);            }
+        }
+    }
+};
+
+twoSum(nums,target);
+
