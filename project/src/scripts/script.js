b84862acd468c2c5c18cc032f2569c02e7d241a1
Zachary <zacharyjolliff15@gmail.com>
2025-04-25 20:55:46 -0400

    added strict equality, better clickInfo, less DOM calls, removed uneeded functions

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,55 +1,41 @@
-//.innerHTML changes everything inside the HTML Box
-//.textContent changes only the text inside the html box
-
-
 let totalClicks = 0;
 let greenClicks = 0;
 let redClicks = 0;
 
-function lighton() {
-    document.getElementById('light').src='/project/src/images/greenlight.jpg';
-    document.getElementById('onOff on').innerText="On"
-    // alert("Light is on!")
-    addOn()
-    clickInfo()
+//save calls to dom, helps memory
+const lightImg = document.getElementById('light');
+const onButton = document.getElementById('onOff on');
+const offButton = document.getElementById('onOff off');
 
-    if (greenClicks  % 2 == 0){
-        document.getElementById('onOff on').innerText="Press for Go"
-    }
+function lighton() {
+    greenClicks++;
+    totalClicks = greenClicks + redClicks;
+    lightImg.src='/project/src/images/greenlight.jpg';
+    // alert("Light is on!")
+    clickInfo()
+    onButton.innerText = greenClicks % 2 === 0 ? "On" : "Press for On";
 }
 
 function lightoff() {
-    document.getElementById('light').src='/project/src/images/redlight.jpg';
-    document.getElementById('onOff off').innerText="Off"
+    redClicks++;
+    totalClicks = greenClicks + redClicks;
+    lightImg.src='/project/src/images/redlight.jpg';
     // window.alert("Light is off!")
-    addOff()
     clickInfo()
-
-    if (redClicks % 2 == 0){
-        document.getElementById('onOff off').innerText="Press for Off"
-    }
+    offButton.innerText = redClicks % 2 === 0 ? "Off" : "Press for Off";
 }
 
 function disappear(){
-    document.getElementById('light').style.display='none';
+    light.style.display='none';
 }
 
 function reappear(){
-    document.getElementById('light').style.display='block';
+    light.style.display='block';
 }
 
 
 function clickInfo(){
-    totalClicks = greenClicks + redClicks;
     console.log("total green clicks: " + greenClicks)
     console.log("total red clicks: " + redClicks)
     console.log("total clicks: " + totalClicks)
-}
-
-function addOn() {
-    greenClicks++;
-}
-
-function addOff(){
-    redClicks++;
 }
