7320370f48d4ae3935f80543d127fb15957af636
Zachary <zacharyjolliff15@gmail.com>
2025-04-29 12:27:20 -0400

    incorrect approach to romanInterger problem, forget about subtraction rule

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -50,35 +50,90 @@ function clickInfo(){
 
 
 //Palindrom problem, first solve as string
-x = 0;
-var isPalindrome = function(x) {
-    const input = x.toString();
-    if (input.length <= 1){
-        console.log("🚀 ~ isPalindrome ~ true:", true)
-        return true;
-    }
+// x = 0;
+// var isPalindrome = function(x) {
+//     const input = x.toString();
+//     if (input.length <= 1){
+//         console.log("🚀 ~ isPalindrome ~ true:", true)
+//         return true;
+//     }
 
-    part1even = input.slice(0,input.length / 2)
-    part2even = input.slice(input.length / 2 , input.length)
-    part2Evenreversed = part2even.split("").reverse().join("")
+//     part1even = input.slice(0,input.length / 2)
+//     part2even = input.slice(input.length / 2 , input.length)
+//     part2Evenreversed = part2even.split("").reverse().join("")
 
-    if (part1even == part2Evenreversed){
-        console.log("🚀 ~ isPalindrome ~ true:", true)
-        return true
-    }
+//     if (part1even == part2Evenreversed){
+//         console.log("🚀 ~ isPalindrome ~ true:", true)
+//         return true
+//     }
    
-    part1odd = input.slice(0,(input.length + 1) / 2)
-    part2odd = input.slice(input.length / 2, input.length)
-    part2Oddreversed = part2odd.split("").reverse().join("")
+//     part1odd = input.slice(0,(input.length + 1) / 2)
+//     part2odd = input.slice(input.length / 2, input.length)
+//     part2Oddreversed = part2odd.split("").reverse().join("")
 
-    if (part1odd == part2Oddreversed){
-        console.log("🚀 ~ isPalindrome ~ true:", true)
-        return true
+//     if (part1odd == part2Oddreversed){
+//         console.log("🚀 ~ isPalindrome ~ true:", true)
+//         return true
+//     }
+
+//     else{
+//         return false
+//     }
+// };
+// isPalindrome(x)
+
+
+//  3)roman numerals
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    for (let i = 0; i < s.length; i++){
+        switch(s[i]){
+            case "I":
+                s[i] = "1"; 
+            break
+
+            case "V":
+                s[i] = "5"; 
+            break
+
+            case "X":
+                s[i] = "10"; 
+            break
+
+            case "L":
+                s[i] = "50"; 
+            break
+
+            case "C":
+                s[i] = "100"; 
+            break
+
+            case "D":
+                s[i] = "500"; 
+            break
+
+            case "M":
+                s[i] = "1000"; 
+            break
+        }
+
+    }
+    let concatStr = "";
+    for (let j = 0; j < s.length; j++){
+        console.log(s)
+        const currentValue = s[j];
+        concatStr += currentValue;
     }
 
-    else{
-        return false
-    }
+    console.log("🚀 ~ romanToInt ~ concatStr:", s)
+
+
 };
-isPalindrome(x)
 
+
+s = "LVIII"
+romanToInt(s)
