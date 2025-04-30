468741dfa4a7810633caf549a63f9e6e641b2f82
Zachary <zacharyjolliff15@gmail.com>
2025-04-30 08:14:04 -0400

    Write a function createHelloWorld. It should return a new function that always returns 'Hello World'

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,31 +1,19 @@
 /**
- * @param {string} s
- * @return {number}
+ * @return {Function}
  */
-var romanToInt = function(s) {
-    const romanToIntObject = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    };
-    let result = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (romanToIntObject[s[i]] < romanToIntObject[s[i + 1]]) {
-            result -= romanToIntObject[s[i]];
-        } else {
-            result += romanToIntObject[s[i]];
-        }
+var createHelloWorld = function() {
+    return function f() {
+        return "Hello world"
     }
-    console.log("🚀 ~ romanToInt ~ result:", result)
-    return result;
-};
-s = "XLIX"
-romanToInt(s)
+ 
+    
+    
+}
 
-/*
-i	s[i]	s[i+1]	value(s[i])	value(s[i+1])	value(s[i])<value(s[i+1])?	result change	running total
-0	X	L	10	50	10 < 50 → true	–10	–10
-1	L	I	50	1	50 < 1 → false	+50	40
-2	I	X	1	10	1 < 10 → true	–1	39
-3	X	undefined	10	undefined	10 < undefined → false	+10	49
 
-*/
+const f = createHelloWorld();
+ f(); // "Hello World"
+ 
+
+//  Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+// 
