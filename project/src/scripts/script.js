61d814bba5894f2945dbd0de7111df13bf8d9433
Zachary <zacharyjolliff15@gmail.com>
2025-04-30 08:39:31 -0400

    Immediately Invoked Function Expression call from memory

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,13 +1,24 @@
-//JS closure example that is really cool
-function createMult(factor){
-    return function (number){
-        return number * factor
-    }
-}  
+// //JS closure example that is hurts my head
 
-const double = createMult(2)
-const triple = createMult(3)
+// function createMult(factor){
+//     return function (number){
+//         return number * factor
+//     }
+// }  
+
+// const double = createMult(2)
+// const triple = createMult(3)
 
 
-console.log(double(5));   // Output: 10
-console.log(triple(5));   // Output: 15
+// console.log(double(5));   // Output: 10
+// console.log(triple(5));   // Output: 15
+
+//JS example that hurts my head called Immediately Invoked Function Expression
+const result = (function(a, b) {const sum = a + b; return sum;})
+
+(1,2)
+console.log(result)
+
+const IIFEsubtract = (function(x,y) {const sub = x - y; return sub})
+(6,2)
+console.log(IIFEsubtract)
