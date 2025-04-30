17ec98b502a66a3d2ae0bad9771871880b73fa77
Zachary <zacharyjolliff15@gmail.com>
2025-04-30 09:40:14 -0400

    spread syntax,

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -14,11 +14,37 @@
 // console.log(triple(5));   // Output: 15
 
 //JS example that hurts my head called Immediately Invoked Function Expression
-const result = (function(a, b) {const sum = a + b; return sum;})
+// const result = (function(a, b) {const sum = a + b; return sum;})
 
-(1,2)
-console.log(result)
+// (1,2)
+// console.log(result)
 
-const IIFEsubtract = (function(x,y) {const sub = x - y; return sub})
-(6,2)
-console.log(IIFEsubtract)
+// const IIFEsubtract = (function(x,y) {const sub = x - y; return sub})
+// (6,2)
+// console.log(IIFEsubtract)
+
+//Arrow Syntax
+// const f = (a, b) => a+b
+// console.log(f(3, 4)); // 7
+
+//spread syntax for array
+const fruits1 = ['apple', 'banana'];
+const fruits2 = ['orange', 'grape'];
+
+const combinedFruitsspread = [...fruits1, ...fruits2]
+console.log(combinedFruitsspread)
+
+const person = { name: 'Alice', age: 30 };
+
+//spread syntax for object
+
+// Without spread syntax, copying can be more manual:
+const personCopy_old = {};
+for (const key in person) {
+  personCopy_old[key] = person[key];
+}
+console.log(personCopy_old); // Output: { name: "Alice", age: 30 }
+
+// With spread syntax, it's very straightforward:
+const personCopy_new = { ...person };
+console.log(personCopy_new); // Output: { name: "Alice", age: 30 }
