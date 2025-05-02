a29202eb242e86a04976bbf4c86537883a3bf239
Zachary <zacharyjolliff15@gmail.com>
2025-05-02 11:59:56 -0400

    ToBeOrNotToBe but with correct error handling

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -1,35 +1,22 @@
 //total hrs studied 16
 
-//9 minutes off
-
 /**
- * @param {string[]} strs
- * @return {string}
+ * @param {string} val
+ * @return {Object}
  */
-
-
- // need to iterate through each string in the array but only as much as the shortest lengthed input
- // should have a common array that is outside of loop and if all 3 have the same letter then push that value into the array,
- var longestCommonPrefix = function(strs) {
-  //emmpty array i will push out answer to
-  const commonArr = [];
-
-  let shortestInputLength = 0;
-  for (i = 0; i < strs.length - 1; i++){
-    if(strs[i].length < strs[i + 1].length){
-      shortestInputLength = strs[i].length;
-    }
-  }
-
-  for (i = 0; i < strs.length; i++){
-    for(j = i + 1; j < strs.length; j++){
-      if(strs[i]){
-
+var expect = function(val) {
+  return {
+      toBe: function(otherVal) {
+          if (val === otherVal) return true;
+          else throw new Error("Not Equal");
+      },
+      notToBe: function(otherVal) {
+          if (val !== otherVal) return true;
+          else throw new Error("Equal");
       }
-    }
-  }
+  };
+};
 
- };
+ console.log("🚀 ~  expect(5).toBe(5);:",  expect(5).toBe(null))
 
- strs = ["flower", "flow", "flights"]
- console.log("🚀 ~  longestCommonPrefix(strs):",  longestCommonPrefix(strs))
+ 
