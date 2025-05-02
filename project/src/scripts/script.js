02bb6bef4cc1b680e468897992d9561517cd81dd
Zachary <zacharyjolliff15@gmail.com>
2025-05-02 11:42:19 -0400

    unsolved longest common prefix

--- project/src/scripts/script.js
+++ project/src/scripts/script.js
@@ -14,11 +14,18 @@
   //emmpty array i will push out answer to
   const commonArr = [];
 
-
   let shortestInputLength = 0;
   for (i = 0; i < strs.length - 1; i++){
     if(strs[i].length < strs[i + 1].length){
       shortestInputLength = strs[i].length;
+    }
+  }
+
+  for (i = 0; i < strs.length; i++){
+    for(j = i + 1; j < strs.length; j++){
+      if(strs[i]){
+
+      }
     }
   }
 
